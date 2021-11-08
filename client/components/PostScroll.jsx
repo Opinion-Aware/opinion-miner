8ac2d46c849e9eccb35ec823dc94d23082595d07
Mark Dolan <markdolan35@LAPTOP-GHBB0I7K.localdomain>
2021-11-07 19:24:32 -0500

    more components for dash

--- client/components/PostScroll.jsx
+++ client/components/PostScroll.jsx
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 
 
 // loop over props.tweets
-// use the MUI list 
 // forEach item of list render the MUI list item
 // display vals for message and username.. or what ever data we wish to render 
 
 export class PostScroll extends Component {
     render() {
         return (
-            <div>
+            <div id='postScroll'>
+                {this.tweets.message.map(message => <SinglePost/>)}
                 
             </div>
         )
